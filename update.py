35cecc8d72a6dded737bf943a57c86fb2efba550
Jason Trigg <jasontrigg0@gmail.com>
2021-01-05 10:53:20 -0500

    Scrape betfair odds once per minute as part of the main update loop

--- update.py
+++ update.py
@@ -5,10 +5,11 @@ import subprocess
 import datetime
 import time
 sys.path.append(os.path.dirname(__file__)) #allow import from current directory
-from scraper import update_nov_3_election_data as update_election_data #MUST: change to jan 5
+from scraper import scrape_betfair_odds, update_nov_3_election_data as update_election_data #MUST: change to jan 5
 from predict import generate_predictions
 
 if __name__ == "__main__":
+    time_since_betfair = 0
     while True:
         print("Checking for new data...")
         found_new_data = update_election_data()
@@ -20,4 +21,10 @@ if __name__ == "__main__":
             subprocess.check_output(["git", "add", os.path.join(os.path.dirname(__file__),'pred.json'), os.path.join(os.path.dirname(__file__),'precinct-pred.json')])
             subprocess.check_output(["git", "commit", "-m", f"Automated prediction update: {datetime.datetime.now()}"])
             subprocess.check_output(["git", "push"])
+        if time_since_betfair > 60:
+            print("Scraping betfair odds")
+            scrape_betfair_odds()
+            time_since_betfair = 0
+        print("Sleeping for 10 seconds")
+        time_since_betfair += 10
         time.sleep(10)
